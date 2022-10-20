5436a5b9fabc604d9e119fd8819cae5d4661c95d
raSel <mdraselm413@gmail.com>
2022-10-20 21:05:57 +0600

    icon

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {FaGoogle, FaGithub} from "react-icons/fa";
 
 const RightSideNav = () => {
-    return (
-        <div>
-            <h2>Right Side Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <ButtonGroup vertical>
+        <Button variant="outline-primary"> <FaGoogle></FaGoogle> Login With Google</Button>
+        <Button variant="outline-dark"><FaGithub></FaGithub> Login With Github</Button>
+      </ButtonGroup>
+    </div>
+  );
 };
 
 export default RightSideNav;
